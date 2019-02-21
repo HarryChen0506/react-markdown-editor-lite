e832a7bc6382f6cca14360dcb30f984361fc3190
HarryChen0506 <chenwenhua0506@sina.com>
2019-02-21 22:15:07 +0800

    fix: UI

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -54,7 +54,7 @@ const config = {
           {
             loader: 'url-loader',
             options: {
-              limit: 10000,
+              limit: 20000,
             }
           }
         ]
