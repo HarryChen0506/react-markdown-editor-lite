3c30a4bea78c9050aaf46d2cc601319516486ba0
ShuangYa <896640547@qq.com>
2019-06-05 21:10:44 +0800

    move browserslist to package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   // parser: 'sugarss',
   plugins: [
-    require('autoprefixer')({
-      "browsers": [
-          "defaults",
-          "not ie < 11",
-          "last 2 versions",
-          "> 1%",
-          "iOS 7",
-          "last 3 iOS versions"
-      ]
-    })
+    require('autoprefixer')
   ]
 }
