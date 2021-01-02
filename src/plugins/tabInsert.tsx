4e6a40f2a2ae83b86d9876684f29f91144dd5bde
ic-e <728272080@qq.com>
2021-01-02 11:54:39 +0800

    Extend config to determine whether to show the switch. Add commit about this plugin.

--- src/plugins/tabInsert.tsx
+++ src/plugins/tabInsert.tsx
@@ -1,16 +1,30 @@
+/**
+ * Since the Markdown Editor will lose input focus when user tpye a Tab key,
+ * this is a built-in plugin to enable user to input Tab character.
+ * Developer can assign its `defaultConfig` to determine
+ * the actually input when user type a Tab key and
+ * whether to display a switch in the toolbar,
+ * see src/demo/index.tsx.
+ */
 import * as React from 'react';
 import { KeyboardEventListener } from '../share/var';
 import { PluginComponent } from './Plugin';
 import i18n from '../i18n';
 
+/**
+ * @field tabMapValue:  Number of spaces will be inputted. Especially, note that 1 means a '\t' instead of ' '.
+ * @field visible:      Whether to display a tab-map-value switch(a <select> tag) in the toolbar.
+ */
 interface TabInsertState {
   tabMapValue: number;
+  visible: boolean;
 }
 export default class TabInsert extends PluginComponent<TabInsertState> {
   static pluginName = 'tab-insert';
   static align = 'right';
   static defaultConfig = {
     tabMapValue: 1,
+    visible: false,
   };
 
   private handleKeyboard: KeyboardEventListener;
@@ -19,6 +33,7 @@ export default class TabInsert extends PluginComponent<TabInsertState> {
     super(props);
     this.state = {
       tabMapValue: this.getConfig('tabMapValue'),
+      visible: this.getConfig('visible'),
     };
     this.handleKeyboard = {
       key: 'Tab',
@@ -45,15 +60,17 @@ export default class TabInsert extends PluginComponent<TabInsertState> {
   }
 
   render() {
-    return (
+    return this.state.visible ? (
       <span title={i18n.get('selectTabMap')}>
-        <select onChange={this.handleSelectChange} defaultValue="1">
+        <select onChange={this.handleSelectChange} defaultValue={this.getConfig('tabMapValue')}>
           <option value="1">1 Tab</option>
           <option value="2">2 Spaces</option>
           <option value="4">4 Spaces</option>
           <option value="8">8 Spaces</option>
         </select>
       </span>
+    ) : (
+      <span />
     );
   }
 }
